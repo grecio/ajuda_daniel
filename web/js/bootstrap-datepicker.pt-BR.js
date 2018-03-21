20a1de8720ab0b78bd29f49560c253d40aeeffd6
SolucaoTributos <grecio@MacBook-Pro-de-grecio.local>
2018-03-21 00:53:30 -0300

    Atuando nos ajustes

--- web/js/bootstrap-datepicker.pt-BR.js
+++ web/js/bootstrap-datepicker.pt-BR.js
@@ -1,13 +1,14 @@
-
 /**
  * Brazilian translation for bootstrap-datepicker
  * Cauan Cabral <cauan@radig.com.br>
  */
 ;(function($){
 	$.fn.datepicker.dates['pt-BR'] = {
-		days: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
-		daysShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
-		daysMin: ["Do", "Se", "Te", "Qu", "Qu", "Se", "Sa"],
+
+		    
+		days: ["Domingo", "2º", "3º", "4º", "5º", "6º", "Sábado"],
+		daysShort: ["Dom", "2º", "3º", "4º", "5º", "6º", "Sáb"],
+		daysMin: ["Do", "2º", "3º", "4º", "5º", "6º", "Sa"],
 		months: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
 		monthsShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
 		today: "Hoje",
